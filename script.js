30985a682c7009f91887f93b0ad4cf06ee25cd4d
vrindaj <vrinda.joshi@hotmail.com>
2024-03-19 22:52:53 -0400

    update date and form

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // CODE FROM https://css-tricks.com/snippets/css/typewriter-effect/
 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
 document.addEventListener('DOMContentLoaded',function(event){
         
     // type one character in the typewriter
@@ -8,7 +17,7 @@ document.addEventListener('DOMContentLoaded',function(event){
       // check if text isn't finished yet
       if (i < (text.length)) {
         // add next character to h1
-       document.querySelector(location).innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+       document.querySelector(location).innerHTML = text.substring(0, i+1) +'<span class="true" aria-hidden="true"></span>';
   
         // wait for a while and call this function again for next character
         setTimeout(function() {
@@ -22,27 +31,3 @@ document.addEventListener('DOMContentLoaded',function(event){
     });
     
   });  
-
-  var slideIndex = 0;
-  carousel();
-  
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 7000); 
-  }
-
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
